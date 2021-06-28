06fa3668dee5410595d538d2a8abd4c46fd41a0e
Foram Patel <86473714+foram147@users.noreply.github.com>
2021-06-28 15:06:42 +0100

    Add files via upload

---
+++ m1d6.js
@@ -0,0 +1,228 @@
+console.log("---------------exA------------")
+let var1 = "foram"
+console.log(var1)
+
+console.log("---------------exB------------")
+let sum 
+ sum = 10+20
+console.log(sum)
+
+console.log("---------------exC------------")
+let  randomNum 
+ randomNum = Math.floor( Math.random()*(20-0))+0;
+console.log(randomNum)
+
+console.log("---------------exD------------")
+let me = {
+    name1: "foram", surname:"patel", age:28,
+};
+console.log(me)
+
+console.log("---------------exE------------")
+delete me.age;
+console.log(me)
+
+console.log("---------------exF------------")
+ me = {skills:["JS","PHP",".NET","JAVA"],};
+console.log(me.skills)
+
+console.log("---------------exG------------")
+me.skills.pop();
+console.log(me.skills)
+
+console.log("----------------------------JS FUNCTIONS--------------------")
+console.log("------------ex1-------------")
+const dice=function(min,max){
+  return Math.floor(Math.random()*(max-min)+min);
+}
+console.log(dice(1,6))
+
+console.log("-----------ex2--------------")
+const whoIsBigger = function(a,b){
+    return a>b? a:b;
+}
+console.log("the bigger num is " ,whoIsBigger(110,20))
+
+console.log("--------------ex3-----------")
+const splitMe = function(strtoadd){
+    let strSplitted = strtoadd.split(" ") ; 
+    return strSplitted;
+}
+console.log(splitMe("i love coding"))
+
+
+console.log("-------------ex4------------")
+const deleteOne = function(str1,a){
+    if (a==true){
+      str1= str1.substring(1); // to remove first character 
+    }
+    else{
+      str1=str1.slice(0,-1); // to remove last character
+    }
+    return str1;
+}
+console.log(deleteOne("abcdef",true))
+
+console.log("---------------ex5-------------")
+const onlyLetters = function(str1){
+    if(str1===""){
+        str1=str1;
+    }
+    else{
+        str1=str1.replace(/[0-9]/g,'');
+    }
+    return str1;
+}
+console.log(onlyLetters("i am 28"))
+
+console.log("----------------ex6--------------")
+function isThisAnEmail(txt) {
+    return txt.length > 5 && txt.indexOf('@')>0 ?  true : false;
+ }
+console.log(isThisAnEmail("pforam147@gmail.com"))
+
+console.log("-----------------ex7-------------")
+//const whatDayIsIt = function(){
+    let today = new Date();
+    let day = today.getDay();
+    let days = ["sunday","monday","tuesday","wednesday","thursday","friday","satuday"];
+    let CurrentDay= days[day];
+//}
+console.log("today is ",CurrentDay)
+
+console.log("-------------ex8-------------")
+
+
+console.log("------------ex9--------------")
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+console.log(datediff('21-6-2021','28-6-2021'));
+
+
+
+console.log("--------------ex10-------------")
+
+console.log("------------------ARRAY------------")
+console.log("----------------------ex1---------------")
+const movies = [
+    {
+      Title: "The Lord of the Rings: The Fellowship of the Ring",
+      Year: "2001",
+      imdbID: "tt0120737",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Return of the King",
+      Year: "2003",
+      imdbID: "tt0167260",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Two Towers",
+      Year: "2002",
+      imdbID: "tt0167261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of War",
+      Year: "2005",
+      imdbID: "tt0399295",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lords of Dogtown",
+      Year: "2005",
+      imdbID: "tt0355702",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings",
+      Year: "1978",
+      imdbID: "tt0077869",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1990",
+      imdbID: "tt0100054",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lords of Salem",
+      Year: "2012",
+      imdbID: "tt1731697",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+      Year: "1984",
+      imdbID: "tt0087365",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1963",
+      imdbID: "tt0057261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Avengers",
+      Year: "2012",
+      imdbID: "tt0848228",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Infinity War",
+      Year: "2018",
+      imdbID: "tt4154756",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Age of Ultron",
+      Year: "2015",
+      imdbID: "tt2395427",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Endgame",
+      Year: "2019",
+      imdbID: "tt4154796",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+    },
+  ]
+  
+ delete movies.Year
+ console.log(movies)
+  
